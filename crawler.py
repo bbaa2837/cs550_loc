d5e87641c3b0b238468ca2a194145450d0e32286
kevins3204 <kevins3204@kaist.ac.kr>
2019-04-30 19:28:59 +0900

    crawler modified due to op.gg error

--- crawler.py
+++ crawler.py
@@ -15,12 +15,13 @@ def crawl_list():
         temp[i] = soup.select(
             'body > div.l-wrap.l-wrap--champion > div.l-container > div.l-champion-index > div.l-champion-index-content > div.l-champion-index-content--main > div.champion-index__champion-list > div:nth-child('+ str(i + 1) +') > a'
         )
-        temp[i] = str(temp[i][0])
-        index_name = temp[i].find('statistics')
-        # index_link = temp[i].find('>')
-        champ_name[i] = temp[i][19:index_name - 1]
-        # champ_link[i] = 'https://op.gg'+temp[i][9:index_link - 1]
-        crawl_img(champ_name[i])
+        if(temp[i] != []):
+            temp[i] = str(temp[i][0])
+            index_name = temp[i].find('statistics')
+            # index_link = temp[i].find('>')
+            champ_name[i] = temp[i][19:index_name - 1]
+            # champ_link[i] = 'https://op.gg'+temp[i][9:index_link - 1]
+            crawl_img(champ_name[i])
 
 def crawl_img(champ_name):
     # url = "http://ddragon.leagueoflegends.com/cdn/9.8.1/img/champion/" + champ_name[0].upper() + champ_name[1:] + ".png"
