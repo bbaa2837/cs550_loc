1ce80b1f0d9e3db3202cfa7849a85ded8fdde26d
bbaa2837 <15015seah@gmail.com>
2019-05-07 13:27:53 +0900

    change db configuration

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 module.exports = {
+    host : '143.248.138.176',
     user : 'root', 
     password : 'password',
     port : '3306',
+    socketPath: '/var/run/mysqld/mysqld.sock',
     database : 'info'
 };
